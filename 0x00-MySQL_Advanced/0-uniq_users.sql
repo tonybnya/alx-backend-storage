c748c9eedc6aa2573139ebd0265e695688a8eaf3
Tony <nya.tony2010@gmail.com>
2023-10-12 20:45:17 +0100

    Update: '0-uniq_users.sql' file

--- 0x00-MySQL_Advanced/0-uniq_users.sql
+++ 0x00-MySQL_Advanced/0-uniq_users.sql
@@ -1,7 +1,6 @@
 -- Task: Create the 'users' table if it does not exist
-
-CREATE TABLE IF NOT EXISTS users (
-  id INT AUTOINCREMENT NOT NULL PRIMARY KEY,
-  email VARCHAR(255) NOT NULL UNIQUE,
-  name VARCHAR(255)
+CREATE TABLE IF NOT EXISTS `users` (
+  `id` INT AUTOINCREMENT NOT NULL PRIMARY KEY,
+  `email` VARCHAR(255) NOT NULL UNIQUE,
+  `name` VARCHAR(255)
 );
