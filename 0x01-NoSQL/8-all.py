0395555ec86f147911443690d3f7172fa8b8df0c
Tony <nya.tony2010@gmail.com>
2023-10-17 13:08:26 +0100

    Add: '8-all.py' file

---
+++ 0x01-NoSQL/8-all.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python3
+"""
+Python function that lists all documents in a collection
+
+- Prototype: def list_all(mongo_collection):
+- Return an empty list if no document in the collection
+- mongo_collection will be the pymongo collection object
+"""
+
+import pymongo
+
+
+def list_all(mongo_collection):
+    """
+    Function
+    """
+    if not mongo_collection:
+        return []
+
+    return list(mongo_collection.find())
