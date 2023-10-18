f631ad14114ab1f9a0a7537b83efcd679a330f7a
Tony <nya.tony2010@gmail.com>
2023-10-19 00:49:12 +0100

    Update: 'exercise.py' file

--- 0x02-redis_basic/exercise.py
+++ 0x02-redis_basic/exercise.py
@@ -3,7 +3,7 @@
 Writing strings to Redis
 """
 
-from typing import Union
+from typing import Callable, Optional, Union
 import redis
 import uuid
 
@@ -25,3 +25,43 @@ class Cache:
         self._redis.set(key, data)
 
         return key
+
+    def get(self, key: str, fn: Optional[Callable] = None) -> Union[str, bytes, int, float]:
+        """
+        Reading from Redis and recovering original type
+        """
+        data = self._redis.get(key)
+
+        if fn:
+            data = fn(data)
+
+        return data
+
+    def get_str(self, key: str) -> str:
+        """
+        Parametrize Cache.get with string conversion
+        """
+        data = self._redis.get(key)
+
+        return data.decode('utf-8')
+
+    def get_int(self, key: str) -> int:
+        """
+        Parametrize Cache.get with integer conversion
+        """
+        data = self._redis.get(key)
+
+        return int(data.decode('utf-8'))
+
+
+cache = Cache()
+
+TEST_CASES = {
+    b"foo": None,
+    123: int,
+    "bar": lambda d: d.decode("utf-8")
+}
+
+for value, fn in TEST_CASES.items():
+    key = cache.store(value)
+    assert cache.get(key, fn=fn) == value
