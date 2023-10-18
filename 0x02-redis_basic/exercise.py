70c7a6584b4718208ebfd3580ba542708dfa7923
Tony <nya.tony2010@gmail.com>
2023-10-19 00:24:56 +0100

    Add: 'exercise.py' file

--- 0x02-redis_basic/exercise.py
+++ 0x02-redis_basic/exercise.py
@@ -2,3 +2,26 @@
 """
 Writing strings to Redis
 """
+
+from typing import Union
+import redis
+import uuid
+
+
+class Cache:
+    """
+    Definition of the class Cache
+    """
+    def __init__(self):
+        """ Initialization """
+        self._redis = redis.Redis()
+        self._redis.flushdb()
+
+    def store(self, data: Union[str, bytes, int, float]) -> str:
+        """
+        This method takes 'data' as argument and returns a string
+        """
+        key = str(uuid.uuid4())
+        self._redis.set(key, data)
+
+        return key
