83f3c764e7cfeb53b6d8470ca60d8e3262f75045
Tony <nya.tony2010@gmail.com>
2023-10-19 01:05:46 +0100

    Update: 'exercise.py' file

--- 0x02-redis_basic/exercise.py
+++ 0x02-redis_basic/exercise.py
@@ -74,16 +74,3 @@ class Cache:
         data = self._redis.get(key)
 
         return int(data.decode('utf-8'))
-
-
-cache = Cache()
-
-TEST_CASES = {
-    b"foo": None,
-    123: int,
-    "bar": lambda d: d.decode("utf-8")
-}
-
-for value, fn in TEST_CASES.items():
-    key = cache.store(value)
-    assert cache.get(key, fn=fn) == value
